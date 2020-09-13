3bf42fb438c12c5de05108a4b1e94db8b970b09d
Doron Behar <doron.behar@gmail.com>
2020-09-13 20:17:40 +0300

    Add mpdscribble to mpd

---
+++ plugins/scrobbler/start-mpdscribble.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while ((1)); do
+	# 2nd level verbosity is used so we'll know it's doing something
+	mpdscribble --no-daemon --log=- --verbose=2
+	echo mpdscribble stopped monitoring mpd, probably due to mpd restarted or failing >&2
+	echo Trying again in 2 seconds >&2
+	sleep 2
+done
