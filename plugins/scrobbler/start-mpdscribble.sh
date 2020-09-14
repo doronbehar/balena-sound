b60e7a1575472d365b188a76b69fd67b93fdeed2
Doron Behar <doron.behar@gmail.com>
2020-09-14 16:07:22 +0300

    scrobble with verbosity 3

--- plugins/scrobbler/start-mpdscribble.sh
+++ plugins/scrobbler/start-mpdscribble.sh
@@ -2,7 +2,7 @@
 
 while ((1)); do
 	# 2nd level verbosity is used so we'll know it's doing something
-	mpdscribble --no-daemon --log=- --verbose=2
+	mpdscribble --no-daemon --log=- --verbose=3
 	echo mpdscribble stopped monitoring mpd, probably due to mpd restarted or failing >&2
 	echo Trying again in 2 seconds >&2
 	sleep 2
